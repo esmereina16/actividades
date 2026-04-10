1ea9576a2d18aa47ae1f81f68bddadf8ac0fc637
esmereina16 <emreina@escuelasproa.edu.ar>
2026-04-10 16:00:13 -0300

    Update 02.py

--- 02.py
+++ 02.py
@@ -1,3 +1,10 @@
-import panda as pd
-df=pd.read_csv('studentsPerfomance.csv')
-print(df['gender'])
+
+import pandas as pd
+df=pd.read_csv('studentsPerformance.csv')
+print((df['gender' , 'lunch']))
+print([['gender']]. head())
+print([['gender']]. head(12))
+print([['gender']]. tail())
+print([['gender']]. tail(12))
+
+print(df[['gender', 'race/ethnicity', 'parental level of education']].head(10))
