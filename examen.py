4f2307a7566b28cc538e4897e10098188f7810b8
esmereina16 <emreina@escuelasproa.edu.ar>
2026-04-14 14:15:46 -0300

    Update examen.py

--- examen.py
+++ examen.py
@@ -9,5 +9,27 @@ print(df[['gender', 'math score' , 'lunch']].tail(8))
 
 
 2)abrir el csv. mostar las ultimans 3 columnas.mostara si hay espacio vacio. mostara la cantidad. mostrar el tipo de dato de la ultima columna 
+import pandas as pd
+df = pd.read_csv('StudentsPerformance.csv')
+
+print(df[['math score','reading score','writing score']])
+
+print(df[['math score','reading score','writing score']].shape)
+
+print(df[['writing score']].dtypes)
+
+print(df.info())
+
+print(df.tail())
+
+
+
 3)abrie csv. mostrar la columna reading score. mostrar la cantidad y mostrar la suma 
+
+import pandas as pd
+df = pd.read_csv('StudentsPerformance.csv')
+
+print(df['reading score'].sum())
+print(df.shape)
+
 4)mostrar las dos primeras columnas. mostrar los tipos de datos. mostrar las ultimas 10 filas.
